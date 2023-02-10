b056e5c8f14bd67c7d0c5d183f4cc187863ede31
jaishiv <106514172+jaishivkumar@users.noreply.github.com>
2023-02-10 13:07:54 +0530

    done

---
+++ app.controller.spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+});
+
+function beforeEach(arg0: () => Promise<void>) {
+  throw new Error('Function not implemented.');
+}
+
+
+function expect(arg0: any) {
+  throw new Error('Function not implemented.');
+}
---
+++ app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserSchema } from './user.models';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://shivkumar:I81Q7TvA9ObKHdC3@cluster0.ub8d9.mongodb.net/anj'),
+    MongooseModule.forFeature([{name:'user',schema:UserSchema}])
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
